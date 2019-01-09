4e4ab34812578a3bb1c1906eec04e9cf11c011ce
Cody Tanner <ctanner@instructure.com>
2019-01-09 15:15:10 -0700

    Style tweaks

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -3,21 +3,26 @@ import React from 'react'
 class Header extends React.Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand" href="#"><strong>CODY</strong>TANNER.COM</a>
-          </div>
-          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+        <div className='container'>
+          <a class="navbar-brand" href="#"><strong>CODY</strong>TANNER.COM</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item"><a href="#carousel-header">Home</a></li>
-              <li class="nav-item"><a href="#about">About</a></li>
-              <li class="nav-item"><a href="#projects">Projects</a></li>
-              <li class="nav-item"><a href="#blog">Blog</a></li>
-              <li class="nav-item"><a href="#social">Social</a></li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
             </ul>
           </div>
         </div>
@@ -27,3 +32,4 @@ class Header extends React.Component {
 }
 
 export default Header;
+
--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -6,6 +6,10 @@ import recent3 from '../images/recent3.jpg'
 import recent4 from '../images/recent4.jpg'
 import recent5 from '../images/recent5.jpg'
 import recent6 from '../images/recent6.jpg'
+import left from '../images/left.png'
+import right from '../images/right.png'
+import slide1 from '../images/slide1.jpg'
+import slide2 from '../images/slide2.jpg'
 
 class Index extends React.Component {
   render() {
@@ -22,104 +26,103 @@ class Index extends React.Component {
             </div>
 
             <div className="item active">
-              <img src="img/slide1.jpg" alt="" />
+              <img src={slide1} alt="" />
             </div>
 
             <div className="item">
-              <img src="img/slide2.jpg" alt="" />
+              <img src={slide2} alt="" />
             </div>
           </div>
 
           <a className="left carousel-control" href="#carousel-header" role="button" data-slide="next">
-            <img src="img/left.png" alt="Previous" />
+            <img src={left} alt="Previous" />
           </a>
           <a className="right carousel-control" href="#carousel-header" role="button" data-slide="next">
-            <img src="img/right.png" alt="Next" />
+            <img src={right} alt="Next" />
           </a>
         </div>
 
-        <div id="about" className="content-block content-block-cyan">
+        <div id="about" className="content-block content-block-cyan">">
           <div className="container">
             <div className="row">
-              <div className="col-md-6 col-md-offset-3">
-                <h1>Cody Tanner Photography</h1>
-                <p>
-                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis.
-                </p>
+              <div class="col-8 mx-auto">
+                <div>
+                  <h1>CodyTanner.com Adventures and Photography</h1>
+                  <p>
+                      Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-
         <div id="projects" className="recent-projects">
-            <div className="recent-projects-title">
-                Recent Projects
-            </div>
+          <div className="recent-projects-title">
+            Recent Projects
+          </div>
 
-            <div className="recent-projects-content content-block-gray">
-                <div id="owl-example" className="owl-carousel">
-                    <div>
-                        <img src={recent1} alt="" />
-                        <a href="#">Cartoon de loop</a>
-                    </div>
-                    <div>
-                        <img src={recent2} alt="" />
-                        <a href="#">In to the snow</a>
-                    </div>
-                    <div>
-                        <img src={recent3} alt="" />
-                        <a href="#">girl in sunset</a>
-                    </div>
-                    <div>
-                        <img src={recent4} alt="" />
-                        <a href="#">Tools</a>
-                    </div>
-                    <div>
-                        <img src={recent5} alt="" />
-                        <a href="#">Sunrise in west</a>
-                    </div>
-                    <div>
-                        <img src={recent6} alt="" />
-                        <a href="#">girl in sunset</a>
-                    </div>
-                    <div>
-                        <img src={recent4} alt="" />
-                        <a href="#">Lorem Ipsum</a>
-                    </div>
-                </div>
+          <div className="recent-projects-content content-block-gray">
+            <div id="owl-example" className="owl-carousel">
+              <div>
+                <img src={recent1} alt="" />
+                <a href="#">Cartoon de loop</a>
+              </div>
+              <div>
+                <img src={recent2} alt="" />
+                <a href="#">In to the snow</a>
+              </div>
+              <div>
+                <img src={recent3} alt="" />
+                <a href="#">girl in sunset</a>
+              </div>
+              <div>
+                <img src={recent4} alt="" />
+                <a href="#">Tools</a>
+              </div>
+              <div>
+                <img src={recent5} alt="" />
+                <a href="#">Sunrise in west</a>
+              </div>
+              <div>
+                <img src={recent6} alt="" />
+                <a href="#">girl in sunset</a>
+              </div>
+              <div>
+                <img src={recent4} alt="" />
+                <a href="#">Lorem Ipsum</a>
+              </div>
             </div>
+          </div>
         </div>
 
-
-
         <div id="blog" className="content-block content-block-gray">
-            <div className="container">
-                <header className="block-heading cleafix">
-                    <a href="#" className="btn btn-o btn-lg pull-right">View All</a>
-                    <h1>From the Blog</h1>
-                    <p>Keep up with the latest happenings.</p>
-                </header>
-                <section className="block-body">
-                    <div className="row">
-                        <div className="col-sm-4 blog-post">
-                            <img src="img/bike_water1-1000x600.jpg" alt="Design Your Mind" />
-                            <a href="#"><h2>Design Your Mind</h2></a>
-                            <div className="date">3 Nov, 2014</div>
-                        </div>
-                        <div className="col-sm-4 blog-post">
-                            <img src="img/mac_glasses1-1000x600.jpg" alt="Winter Is Comming" />
-                            <a href="#"><h2>Winter Is Comming</h2></a>
-                            <div className="date">3 Nov, 2014</div>
-                        </div>
-                        <div className="col-sm-4 blog-post">
-                            <img src="img/road-1000x600.jpg" alt="The Illustration" />
-                            <a href="#"><h2>The Illustration</h2></a>
-                            <div className="date">3 Nov, 2014</div>
-                        </div>
-                    </div>
-                </section>
-            </div>
+          <div className="container">
+            <header className="block-heading cleafix">
+              <a href="#" className="btn btn-o btn-lg pull-right">View All</a>
+              <h1>From the Blog</h1>
+              <p>Keep up with the latest happenings.</p>
+            </header>
+            <section className="block-body">
+              <div className="row">
+                <div className="col-md blog-post">
+                  <img src="img/bike_water1-1000x600.jpg" alt="Design Your Mind" />
+                  <a href="#"><h2>Design Your Mind</h2></a>
+                  <div className="date">3 Nov, 2014</div>
+                </div>
+                <div className="col-md blog-post">
+                  <img src="img/mac_glasses1-1000x600.jpg" alt="Winter Is Comming" />
+                  <a href="#"><h2>Winter Is Comming</h2></a>
+                  <div className="date">3 Nov, 2014</div>
+                </div>
+                <div className="col-md blog-post">
+                  <img src="img/road-1000x600.jpg" alt="The Illustration" />
+                  <a href="#"><h2>The Illustration</h2></a>
+                  <div className="date">3 Nov, 2014</div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     );
